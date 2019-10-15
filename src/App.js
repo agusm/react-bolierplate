3b2ec3a010b811818226d94e23ac527858173249
Agus Miswanto <agusmiswanto16@gmail.com>
2019-10-15 15:46:07 +0700

    add react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Button } from 'reactstrap';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button>Hai</Button>
+      </div>
+    )
+  }
+}
